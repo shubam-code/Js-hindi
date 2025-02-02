fb3b013f9ca3bdd9462b03271659733500c0f488
shubam-code <shubamsharma00009@gmail.com>
2025-02-02 16:26:52 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -7,3 +7,24 @@ console.log( 3
     3 ) // code readability should be high
 
 console.log( "shubu")
+
+
+let name = "shubam"
+let age = 24
+let isloggedin = false
+
+
+// number => 2 to power 53
+// Bigint => for big numbers
+// string => ""
+// Boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+// all are primitive datatypes
+
+// object
+
+console.log(typeof null); // object
+console.log( typeof undefined); // undefined
+
