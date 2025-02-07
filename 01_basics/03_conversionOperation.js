b1351329a0e00398cd4a20739e237f5d3dc71c7c
shubam-code <shubamsharma00009@gmail.com>
2025-02-07 12:39:11 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,6 +24,57 @@ let booleanIsLoggedin = Boolean(isloggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 // all these dataconversions we have learned  through investigation studies.
+// ***************************** Operations ***********************************************************************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2**3);
+// console.log(2/3);
+//  console.log(2%3);
+
+// how we can join two strings
+let str1 = "hello"
+let str2 = " shubam"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
