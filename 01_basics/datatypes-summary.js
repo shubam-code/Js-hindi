5e413eef3d45b70a5cab05d3931aa31243cdaf40
shubam-code <shubamsharma00009@gmail.com>
2025-02-10 15:07:59 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types : string, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+// console.log(id === anotherId);
+
+// const bigNumber = 345676475674676554n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "shubam"
+    age: 22,
+}
+
+const myFunction = function () {
+     console.log("Hello World");
+}
+
+console.log(typeof scoreValue);
