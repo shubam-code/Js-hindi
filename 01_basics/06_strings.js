f22802529978072652ca1ae8374f9bff83ec076f
shubam-code <shubamsharma00009@gmail.com>
2025-02-13 04:00:34 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,16 @@
+const name = "shubam"
+const repoCount = 50
+
+// console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hiteshhc')
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf ('t'));
+
+
+
+
