8283101e03577099bcbc8d643358e1d8647c1c0a
shubam-code <shubamsharma00009@gmail.com>
2025-03-22 14:31:47 +0000

    objects part2

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -29,7 +29,7 @@ JsUser.greeting = function(){
     console.log("Hello JS user");
 }
 JsUser.greetingTwo = function(){
-    console.log( `Hello JS user, ${this.name}`);
+    console.log( `Hello JS user ${this.name}`);
 }
 console.log(JsUser.greeting());
 console.log(JsUser.greetingTwo());
---
+++ 02_basics/04_objects.js
@@ -0,0 +1,57 @@
+// const tinderUser = new Object() -> it is a single ton object 
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Shibu"
+tinderUser.isLoggedIn = false 
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "somae@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Shubam",
+            lastname: "Sharma",
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+// how to combine objects like we did in arrays 
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2, ...obj4}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "s@gmail.com"
+    },
+    {
+        id: 1,
+        email: "s@gmail.com"
+    },
+    {
+        id: 1,
+        email: "s@gmail.com"
+    },
+]
+
+users[1].email,
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
