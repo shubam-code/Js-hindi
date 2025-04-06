12842cf70e0412306be449f0707e0cbb9acf8a6f
shubam-code <shubamsharma00009@gmail.com>
2025-04-06 14:37:10 +0000

    arrow functions

---
+++ 03_basics/03_arrows.js
@@ -0,0 +1,17 @@
+const user = {
+    username: "shubam",
+    price: "999",
+
+    welcomeMessage: function(){
+        console.log(`${this.username},welcome to website`);
+        console.log(this);
+        
+        
+    }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
