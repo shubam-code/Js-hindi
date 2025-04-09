21dd64bd37ef40337f9ae0c13106c4b4a9e8d741
shubam-code <shubamsharma00009@gmail.com>
2025-04-09 19:24:13 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,9 +1,13 @@
-// Immediately invoked Function Expressions (IIFE)
+// Immediately invoked Function Expressions (IIFE) we use it to avoid the main function scope from pollution of global scope 
 
 (function chai(){
+    // named IIFE
     console.log(`DB CONNECTED`);
     
-})()
+})();
 
-chai()
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}` );
+    
+})('shubam')
 
