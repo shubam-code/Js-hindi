b6f8f3a42be7a9b61880d5e8cbb4b9081039edd0
shubam-code <shubamsharma00009@gmail.com>
2025-04-09 13:10:57 +0000

    arrow functions

--- 03_basics/03_arrows.js
+++ 03_basics/03_arrows.js
@@ -15,3 +15,33 @@ const user = {
 // user.welcomeMessage()
 
 // console.log(this);
+
+// function chai(){
+//     let username = "shubam"
+//     console.log(this.username);
+    
+// }
+
+// chai()
+
+// const chai = function(){
+//     let username = "shubam"
+//     console.log(this.username);
+    
+// }
+
+const chai = () => {
+    let username = "shubam"
+    console.log(this.username);  
+}
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// } these are two ways of declearing a arrow function
+// const addTwo = (num1, num2) =>  (num1 + num2) this is implicit return in which we expect to return a value without using return keyword
+
+const addTwo = (num1, num2) =>  ({username: "shubam"})
+
+console.log(addTwo(3, 4))
