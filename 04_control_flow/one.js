3960e539ec4a49f716aa7cdbb39a66ad131d4b1a
shubam-code <shubamsharma00009@gmail.com>
2025-04-10 13:21:47 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,12 @@
+//  if
+const isuserLoggedIn = true
+const temperature = 41
+
+if(temperature === 40){
+    console.log("less than 50");
+} else {
+console.log("more than 50");}
+
+console.log("execute");
+
+// < , >, <=, =>, ==, !=, ===
